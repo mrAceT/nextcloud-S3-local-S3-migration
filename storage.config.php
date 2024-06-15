8ddfe4c9c2469e3f4e2e8fa87d5a8f4728cf718f
Marvin Dickhaus <2642714+Weishaupt@users.noreply.github.com>
2024-06-15 19:21:34 +0200

    Update storage.config.php

--- storage.config.php
+++ storage.config.php
@@ -7,11 +7,11 @@ $CONFIG = array(
                   'key' => '**key', // your key
                   'secret' => '**secret**', // your secret
                   'hostname' => '**host**', // your host
-                  'port' => 443,
-                  'use_ssl' => true,
-                  'use_path_style' => false,
-                  'sse_c_key' => '***sse-c key***', //only provide, if you want to use customer provided encryption key see: https://docs.nextcloud.com/server/latest/admin_manual/configuration_files/primary_storage.html#s3-sse-c-encryption-support
-                  'region' => 'eu-central' //must be provided, even if non-S3 storage is selected. Otherwise the script localtoS3 will fail
+                  'port' => 443, // default
+                  'use_ssl' => true, // default
+                  'use_path_style' => false, //required to be true by some s3 providers
+                  'sse_c_key' => '***sse-c key***', // only provide, if you want to use customer provided encryption key see, otherwise delete: https://docs.nextcloud.com/server/latest/admin_manual/configuration_files/primary_storage.html#s3-sse-c-encryption-support
+                  'region' => '***region***' // must be present, even if non-S3 storage is selected. Otherwise the script localtoS3 will fail
           ),
   ),
 );
